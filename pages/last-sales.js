51967ce1daedf2bc6517ded23c3f6711e0e0767a
huan_rozb <ivanrozb4225@gmail.com>
2023-01-24 17:42:31 +0200

    useSWR

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,33 +1,33 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export default function LastSalesPage() {
     const [sales, setSales] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+    const fetcher = (url) => fetch(url).then((res) => res.json());
+    const { data, error } = useSWR(
+        "https://nextjs-course-86a6c-default-rtdb.firebaseio.com/sales.json",
+        fetcher
+    );
 
     useEffect(() => {
-        setIsLoading(true);
-        fetch(
-            "https://nextjs-course-86a6c-default-rtdb.firebaseio.com//sales.json"
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                const transformedSales = [];
+        if (data) {
+            const transformedSales = [];
 
-                for (const key in data) {
-                    transformedSales.push({
-                        id: key,
-                        username: data[key].username,
-                        volume: data[key].volume,
-                    });
-                }
+            for (const key in data) {
+                transformedSales.push({
+                    id: key,
+                    username: data[key].username,
+                    volume: data[key].volume,
+                });
+            }
 
-                setSales(transformedSales);
-                setIsLoading(false);
-            });
-    }, []);
+            setSales(transformedSales);
+        }
+    }, [data]);
 
-    if (isLoading) return <p>Loading ...</p>;
-    if (sales.length === 0) return <p>No data yet...</p>;
+    if (error) return <p>Error!</p>;
+    if (!data || !sales) return <p>Loading...</p>;
+
     return (
         <ul>
             {sales.map((sale) => (
