2067af785e6f552048a4a8dd9f342ec7296215cf
huan_rozb <ivanrozb4225@gmail.com>
2023-01-24 11:35:40 +0200

    Initial commit from Create Next App

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+  swcMinify: true,
+}
 
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
